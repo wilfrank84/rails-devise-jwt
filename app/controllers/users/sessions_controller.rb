995516399c1245267777cf847cc9f6aa072da8b3
willFrank <william.souza@raroprestador.com.br>
2023-05-27 07:15:46 -0300

    feat: adjustment to rails 7

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  include RackSessionFix
   # before_action :configure_sign_in_params, only: [:create]
   respond_to :json
 
